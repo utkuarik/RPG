e0777e80270274f34535996dd9e44f4db16cdede
Utku Arık <utkuarik@sabanciuniv.edu>
2022-09-07 23:54:40 +0300

    Player moovenent and some UI scripts

--- Codes/PlayerMovement.cs
+++ Codes/PlayerMovement.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    private UnityEngine.AI.NavMeshAgent nav;
+    private NavMeshAgent nav;
     private Animator anim;
     private Ray ray;
     private RaycastHit hit;
+    private AnimatorStateInfo playerInfo;
     private float x;
     private float z;
     private float velocitySpeed;
@@ -18,44 +20,59 @@ public class PlayerMovement : MonoBehaviour
     public CinemachineVirtualCamera playerCam;
     private Vector3 pos;
     private Vector3 currPos;
-
+    public bool CanAttack;
+    public bool IsAttacking;
+    public float AttackCoolDown;
+    private Vector3 initialPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        nav = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
         ct = playerCam.GetCinemachineComponent<CinemachineTransposer>();
         currPos = ct.m_FollowOffset;
-    }
+        initialPos = ct.m_FollowOffset;
+        CanAttack = true;
+        AttackCoolDown = 4.0f;
+        IsAttacking = false;
+    } 
 
-    // Update is called once per frame
+
+    // Start is called before the first frame update
     void Update()
     {
+        playerInfo = anim.GetCurrentAnimatorStateInfo(0);
 
         x = nav.velocity.x;
         z = nav.velocity.z;
         velocitySpeed = x + z;
 
+        Debug.Log(CanAttack);
 
         // Get mouse position
         pos = Input.mousePosition;
         ct.m_FollowOffset = currPos;
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && playerInfo.IsTag("NonAttack"))
         {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray,out hit))
+            if(Physics.Raycast(ray, out hit))
             {
                 nav.destination = hit.point;
+                
             }
+            anim.SetBool("attack", false);
         }
 
         if (Input.GetMouseButton(1))
         {
-            if(pos.x !=0 || pos.y != 0 || pos.z != 0)
-            {
-                currPos = pos / 200;
+            if(pos.x !=0 || pos.y != 0 )
+            {   
+
+                currPos = initialPos;
+
+                Debug.Log(currPos);
             }
         }
 
@@ -63,8 +80,17 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKeyDown("space"))
         {
-            anim.SetBool("attack", true);
+            if (CanAttack)
+            {
+                Debug.Log(CanAttack);
+                SwordAttack();
+                
+            }
+            
+            //anim.SetBool("sprinting", false);
         }
+
+        
 
         if (velocitySpeed != 0)
         {
@@ -76,4 +102,23 @@ public class PlayerMovement : MonoBehaviour
              anim.SetBool("sprinting", false);
         }
      }
+    
+    public void SwordAttack()
+    {
+        Debug.Log(CanAttack);
+        CanAttack = false;
+        anim.SetTrigger("attack");
+        StartCoroutine(ResetAttackCooldown());
+
+    }
+
+    IEnumerator ResetAttackCooldown()
+    {
+        IsAttacking = true;
+        yield return new WaitForSeconds(AttackCoolDown);
+        anim.SetBool("attack", false);
+        CanAttack = true;
+        IsAttacking = false;
+        //gameObject.transform.position = v3_Original + new Vector3(3, 0, 0);
+    }
 }
